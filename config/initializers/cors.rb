eb2bafaab4debbcd3ae757b6664b0232b386868f
Ademar Tutor <hey@ademartutor.com>
2025-01-25 16:35:43 +1300

    add new domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ "http://localhost:3001", "https://agilityautomation.ademartutor.com" ]
+    origins [ "http://localhost:3001", "https://agilityautomation.ademartutor.com", "https://agilityapp.ademartutor.com" ]
 
     resource "*",
       headers: :any,
